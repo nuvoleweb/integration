ff58e6ad933fcf8646f26ed155e4b671d738d36c
Antonio De Marco <antonio@nuvole.org>
2015-09-10 12:56:13 +0200

    Initial work on producer configuration form.

--- modules/integration_producer_ui/src/ProducerFormHandler.php
+++ modules/integration_producer_ui/src/ProducerFormHandler.php
@@ -8,6 +8,7 @@
 namespace Drupal\integration_producer_ui;
 
 use Drupal\integration_ui\AbstractFormHandler;
+use Drupal\integration\Backend\Configuration\BackendConfiguration;
 
 /**
  * Class ProducerFormHandler.
@@ -20,8 +21,33 @@ class ProducerFormHandler extends AbstractFormHandler {
    * {@inheritdoc}
    */
   public function form(array &$form, array &$form_state, $op) {
-    $form['type'] = $this->getFormRadios(t('Producer type'), '', TRUE);
-    $this->componentsForm($form, $form_state, $op);
+    /** @var BackendConfiguration $configuration */
+    $configuration = $this->getConfiguration();
+
+    $options = $this->getPluginManager()->getFormOptions();
+    $form['type'] = $this->getSelect(t('Type'), $options);
+
+    if ($entity_type = $this->getFormValue($form_state, 'type')) {
+      $form['options'] = $this->getFieldset(t('Options'));
+
+      $info = entity_get_info($this->getFormValue($form_state, 'type'));
+      $options = $this->extractSelectOptions($info['bundles'], 'label');
+      $form['options']['bundle'] = $this->getSelect(t('Bundle'), $options);
+    }
+
+    if ($bundle = $this->getFormValue($form_state, 'bundle')) {
+
+      $rows = array();
+      $fields = field_info_instances($entity_type, $bundle);
+      foreach ($fields as $field_name => $field) {
+        $row = array();
+        $row[$field_name . '_source'] = $this->getTextField($field['label']);
+        $row[$field_name . '_destination'] = $this->getTextField(t('Destination'));
+        $rows[$field_name . '_row'] = $row;
+      }
+
+      $form['options']['fields'] = $this->getFormTable(array(t('Source'), t('Destination')), $rows);
+    }
   }
 
 }
