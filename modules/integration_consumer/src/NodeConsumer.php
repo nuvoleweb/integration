c724fc55e2e5c6e9d545585f748dd22ee806dae7
Antonio De Marco <antonio@nuvole.org>
2015-09-12 21:31:48 +0200

    Add node consumer plugin implementation.

---
+++ modules/integration_consumer/src/NodeConsumer.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\integration_consumer\NodeConsumer.
+ */
+
+namespace Drupal\integration_consumer;
+
+/**
+ * Class NodeConsumer.
+ *
+ * @package Drupal\integration_consumer
+ */
+class NodeConsumer extends AbstractConsumer {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $arguments) {
+
+    // @todo: Make the following an option set via UI.
+    $this->addFieldMapping('promote')->defaultValue(FALSE);
+    $this->addFieldMapping('status')->defaultValue(NODE_NOT_PUBLISHED);
+
+    parent::__construct($arguments);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDestinationClass() {
+    return '\MigrateDestinationNode';
+  }
+
+}
