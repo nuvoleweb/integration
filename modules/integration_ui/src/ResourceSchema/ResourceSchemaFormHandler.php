39011bd40e91d637a89cd28a8ac9b8314f40a8e3
Antonio De Marco <antonio@nuvole.org>
2015-09-13 14:03:37 +0200

    Add ResourceSchema form handler.

---
+++ modules/integration_ui/src/ResourceSchema/ResourceSchemaFormHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\integration_ui\ResourceSchema\ResourceSchemaFormHandler
+ */
+
+namespace Drupal\integration_ui\ResourceSchema;
+
+use Drupal\integration_ui\AbstractFormHandler;
+
+class ResourceSchemaFormHandler extends AbstractFormHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array &$form, array &$form_state, $op) {
+    $form['plugin'] = $this->getFormRadios(t('Resource schema plugin'), '', TRUE);
+    $this->componentsForm($form, $form_state, $op);
+  }
+
+}
