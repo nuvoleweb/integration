f190c59411d17fbe631114bb96cbe8b6a6816b2c
Antonio De Marco <antonio@nuvole.org>
2015-10-02 15:41:27 +0200

    FormHelper class cleanup.

--- modules/integration_ui/src/FormHelper.php
+++ modules/integration_ui/src/FormHelper.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Drupal\integration_ui\FormHelper
+ * Contains \Drupal\integration_ui\FormHelper.
  */
 
 namespace Drupal\integration_ui;
@@ -113,7 +113,7 @@ class FormHelper {
    * @param array $array
    *    Two-levels array to extract select options from.
    * @param string $label_key
-   *    Array key of array item to be used as label.
+   *    Array item's key to be used as label.
    *
    * @return array
    *    Input array formatted to be consumable by a Form API select.
@@ -158,18 +158,22 @@ class FormHelper {
    *    Form element label.
    * @param bool|FALSE $tree
    *    Weather the form element is to be treated as a tree.
+   * @param string $group
+   *    Fieldset group, if any.
    *
    * @return array
    *    Form element array as expected by Drupal's Form API.
    */
-  static public function fieldset($label, $tree = FALSE) {
-    return array(
+  static public function fieldset($label, $tree = FALSE, $group = '') {
+    $element = array(
       '#type' => 'fieldset',
       '#title' => $label,
-      '#collapsable' => TRUE,
-      '#collapsed' => TRUE,
       '#tree' => $tree,
     );
+    if ($group) {
+      $element['#group'] = $group;
+    }
+    return $element;
   }
 
   /**
@@ -210,7 +214,35 @@ class FormHelper {
     );
   }
 
-  // -------------------------------------------------------------
+  /**
+   * Form API helper: return hidden form element.
+   *
+   * @param string $value
+   *    Hidden form element value.
+   *
+   * @return array
+   *    Form element array as expected by Drupal's Form API.
+   */
+  static public function hidden($value) {
+    return array(
+      '#value' => $value,
+    );
+  }
+
+  /**
+   * Form API helper: return markup form element.
+   *
+   * @param string $markup
+   *    Markup value.
+   *
+   * @return array
+   *    Form element array as expected by Drupal's Form API.
+   */
+  static public function markup($markup) {
+    return array(
+      '#markup' => $markup,
+    );
+  }
 
   /**
    * Form API helper: return text field element.
@@ -225,7 +257,7 @@ class FormHelper {
    * @return array
    *    Form element array as expected by Drupal's Form API.
    */
-  static public function formTextField($label, $default = NULL, $required = TRUE) {
+  static public function textField($label, $default = NULL, $required = TRUE) {
     return array(
       '#type' => 'textfield',
       '#title' => $label,
@@ -235,137 +267,38 @@ class FormHelper {
   }
 
   /**
-   * Set the dependent field based on the dependee value via AJAX.
+   * Format current PluginManager::getInfo() results as list of radio buttons.
    *
-   * @param array $form
-   *    Current form array.
-   * @param mixed $dependee
-   *    Dependee element name or array of parents + element name if nested.
-   * @param mixed $dependent
-   *    Target element name or array of parents + element name if nested.
-   */
-  static public function setAjaxDependency(array &$form, $dependee, $dependent) {
-
-    $form[$dependee]['#ajax'] = array(
-      'callback' => 'integration_ui_form_ajax_callback',
-      'wrapper' => 'integration-ui-ajax-wrapper-' . $dependent,
-      'target' => $dependent,
-    );
-    $form[$dependent]['#prefix'] = "<div id='integration-ui-ajax-wrapper-$dependent'>";
-    $form[$dependent]['#suffix'] = "</div>";
-  }
-
-  /**
-   * Get form value from $form_state['values'] array.
-   *
-   * @param array $form_state
-   *    Form state array.
-   * @param string $name
-   *    Form state value name.
-   *
-   * @return mixed|FALSE
-   *    Return form value if any, FALSE otherwise.
-   */
-  static public function getFormValue(array &$form_state, $name) {
-    if (isset($form_state['values'][$name])) {
-      return $form_state['values'][$name];
-    }
-    return FALSE;
-  }
-
-  /**
-   * Extract select options from a two-levels array.
-   *
+   * @param string $title
+   *    Form element #title.
    * @param array $array
-   *    Two-levels array to extract select options from.
+   *    Two-levels array to extract options and descriptions from.
+   * @param mixed $default_value
+   *    Form element #default_value.
+   * @param bool|FALSE $required
+   *    Form element #required.
    * @param string $label_key
-   *    Array key of array item to be used as label.
+   *    Array item's key to be used as label.
+   * @param string $description_key
+   *    Array item's key to be used as description.
    *
    * @return array
-   *    Input array formatted to be consumable by a Form API select.
+   *    Form API radio buttons element.
    */
-  static public function extractSelectOptions(array $array, $label_key) {
-    $values = array();
-    foreach ($array as $key => $value) {
-      $values[$key] = $value[$label_key];
-    }
-    return $values;
-  }
+  static public function radios($title, array $array, $default_value, $required = FALSE, $label_key = 'label', $description_key = 'description') {
+    $options = self::asOptions($array, $label_key);
 
-//  /**
-//   * Format current PluginManager::getInfo() results as list of radio buttons.
-//   *
-//   * @param string $title
-//   *    Form element #title.
-//   * @param mixed $default_value
-//   *    Form element #default_value.
-//   * @param bool|FALSE $requiredw
-//   *    Form element #required.
-//   *
-//   * @return array
-//   *    Form API radio buttons element.
-//   */
-//  static public function getFormRadios($title, $default_value, $required = FALSE) {
-//    $options = $this->getPluginManager()->getFormOptions();
-//
-//    $element = array(
-//      '#type' => 'radios',
-//      '#title' => $title,
-//      '#default_value' => $default_value,
-//      '#options' => $options,
-//      '#required' => $required,
-//    );
-//    foreach (array_keys($options) as $name) {
-//      $element[$name] = array('#description' => $this->getPluginManager()->getDescription($name));
-//    }
-//    return $element;
-//  }
-//
-//  /**
-//   * Return current plugin components form portion.
-//   *
-//   * @param array $form
-//   *    Form array.
-//   * @param array $form_state
-//   *    Form state array.
-//   * @param string $op
-//   *    Current form operation.
-//   */
-//  static public function componentsForm(array &$form, array &$form_state, $op) {
-//    $plugin = $this->getPluginManager();
-//
-//    $form['component'] = array(
-//      '#type' => 'vertical_tabs',
-//      '#tree' => FALSE,
-//    );
-//    foreach ($plugin->getComponents() as $component) {
-//
-//      $plugin->setComponent($component);
-//      $label = $plugin->getComponentLabel($component);
-//
-//      $form["component_$component"] = array(
-//        '#type' => 'fieldset',
-//        '#title' => $label,
-//        '#collapsible' => TRUE,
-//        '#group' => 'component',
-//      );
-//      $form["component_$component"][$component] = $this->getFormRadios($label, '', TRUE);
-//
-//      foreach ($plugin->getInfo() as $type => $info) {
-//        $element = array(
-//          '#type' => 'fieldset',
-//          '#title' => t('@component options', array('@component' => $label)),
-//          '#collapsible' => TRUE,
-//          '#group' => "component_$component",
-//        );
-//
-//        $form_manager = FormFactory::getInstance($this->getConfiguration(), $component, $type);
-//        if ($form_manager) {
-//          $form_manager->form($element, $form_state, $op);
-//          $form["component_$component"]["{$component}_configuration"] = $element;
-//        }
-//      }
-//    }
-//  }
+    $element = array(
+      '#type' => 'radios',
+      '#title' => $title,
+      '#default_value' => $default_value,
+      '#options' => $options,
+      '#required' => $required,
+    );
+    foreach ($array as $name => $values) {
+      $element[$name] = array('#description' => $values[$description_key]);
+    }
+    return $element;
+  }
 
 }
