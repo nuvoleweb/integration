4ce04cd235aa6bfe022ea94bff3f2813d1385e65
Antonio De Marco <antonio@nuvole.org>
2015-10-02 15:42:47 +0200

    Add UndefinedFormHandlerException exception calss.

---
+++ modules/integration_ui/src/Exceptions/UndefinedFormHandlerException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\integration_ui\Exceptions\UndefinedFormHandlerException.
+ */
+
+namespace Drupal\integration_ui\Exceptions;
+
+/**
+ * Class UndefinedFormHandlerException.
+ *
+ * @package Drupal\integration_ui\Exceptions
+ */
+class UndefinedFormHandlerException extends \InvalidArgumentException {
+
+}
